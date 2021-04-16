31ead7bbce21ac812c45a67239af50e78a6cad0b
FourSqweez <four7518@gmail.com>
2021-04-16 15:27:40 +0700

    3. Icon, Icon in button

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -3,6 +3,8 @@ import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import ButtonGroup from '@material-ui/core/ButtonGroup'
 import Container from '@material-ui/core/Container'
+import SendIcon from '@material-ui/icons/Send'
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight'
 
 export default function Create() {
   return (
@@ -16,25 +18,17 @@ export default function Create() {
         Create aNew Note
       </Typography>
 
-      <Button type="submit">Submit</Button>
-      <Button type="submit" color="secondary" variant="outlined">
-        Submit
-      </Button>
-
       <Button
         onClick={() => console.log('you clicked me')}
         type="submit"
         variant="contained"
         color="secondary"
+        endIcon={<KeyboardArrowRightIcon />}
       >
         Submit
       </Button>
-
-      <ButtonGroup color="secondary" variant="contained">
-        <Button>One</Button>
-        <Button>Two</Button>
-        <Button>Three</Button>
-      </ButtonGroup>
+      {/* icons */}
+      <br />
     </Container>
   )
 }
