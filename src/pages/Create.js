825909993c1f9d2712f1b8b4bbf3381d3cbefcb7
FourSqweez <four7518@gmail.com>
2021-04-16 15:04:29 +0700

    1. Typography components

---
+++ src/pages/Create.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Typography from '@material-ui/core/Typography'
+
+export default function Create() {
+  return (
+    <div>
+      <Typography
+        variant="h6"
+        component="h2"
+        gutterBottom
+        color="textSecondary"
+      >
+        Create aNew Note
+      </Typography>
+    </div>
+  )
+}
