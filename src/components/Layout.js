11f545317fa5fd9df9c05dd54345d1b096a6ea44
FourSqweez <four7518@gmail.com>
2021-04-19 13:16:34 +0700

    13. Lists & List items

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,12 @@
 import { makeStyles } from '@material-ui/core'
 import Drawer from '@material-ui/core/Drawer'
 import Typography from '@material-ui/core/Typography'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
+import { AddCircleOutlined, SubjectOutlined } from '@material-ui/icons'
+import { useHistory, useLocation } from 'react-router-dom'
 
 const drawerWidth = 240
 
@@ -15,10 +21,28 @@ const useStyles = makeStyles({
   root: {
     display: 'flex',
   },
+  active: {
+    background: '#f4f4f4',
+  },
 })
 
 export default function Layout({ children }) {
   const classes = useStyles()
+  const history = useHistory()
+  const location = useLocation()
+
+  const menuItems = [
+    {
+      text: 'My Notes',
+      icon: <SubjectOutlined color="secondary" />,
+      path: '/',
+    },
+    {
+      text: 'Create Notes',
+      icon: <AddCircleOutlined color="secondary" />,
+      path: '/create',
+    },
+  ]
   return (
     <div className={classes.root}>
       {/* app bar */}
@@ -33,6 +57,21 @@ export default function Layout({ children }) {
         <div>
           <Typography variant="h5">Four Notes</Typography>
         </div>
+
+        {/* list / links */}
+        <List>
+          {menuItems.map((item) => (
+            <ListItem
+              button
+              key={item.text}
+              onClick={() => history.push(item.path)}
+              className={location.pathname == item.path ? classes.active : null}
+            >
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.text} />
+            </ListItem>
+          ))}
+        </List>
       </Drawer>
 
       <div className={classes.page}>{children}</div>
